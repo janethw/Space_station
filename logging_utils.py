aff8dba609c39652611b978eab270194e23aee0d
Janet Hamilton-Wilks <165585567+janethw@users.noreply.github.com>
2024-08-09 23:22:42 +0200

    Notes on logging added and start to build out logging configuration

--- logging_utils.py
+++ logging_utils.py
@@ -1,10 +1,22 @@
 import logging
 
 
-# class :
-#     # Configure Flask logging
-#     # Set log level to INFO
-#     app.logger.setLevel(logging.INFO)
-#     # Log to a file
-#     handler = logging.FileHandler('app.log')
-#     app.logger.addHandler(handler)
+# LOGGING IN FLASK
+# Use the app.logger object once the Flask app is set up
+
+# HANDLERS
+# The log handler sets out how a log is displayed or written.
+# two key fields in log handler are: log formatter, which gives context information
+# and the log level, which filters out logs of an inferior level.
+# Console display with StreamHandler, write to a file with FileHandler
+
+# TO DO: set up the logger for the Flask app
+
+
+def logger_setup():
+    # Configure Flask logging
+    # Set log level to INFO
+    app.logger.setLevel(logging.INFO)
+    # Log to a file
+    handler = logging.FileHandler('app.log')
+    app.logger.addHandler(handler)
