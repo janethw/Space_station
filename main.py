54c454faf3665a2e3bf65298c9a44d188aa38a72
Janet Hamilton-Wilks <165585567+janethw@users.noreply.github.com>
2024-08-11 23:36:06 +0100

    Added comments about turning off the reloader in the app.run statement to prevent Flask's Werkzeug running the script twice as part of its default DEBUG setup, and which might have been leading to duplication in the log statements.

--- main.py
+++ main.py
@@ -7,4 +7,12 @@ app = create_app()
 
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    # Turning off the reloader stops Flask's built-in Werkzeug server,
+    # which runs in debug mode by default, and so by default launches
+    # the server twice: once for the actual server and once for reloader.
+    # Turning off the reloader prevents the script from running twice.
+    # app.run(debug=True, use_reloader=False)
+
+    # However, for dev, we want the relaoder to run, wo will leave it on,
+    # even if it does mean some duplicate logs.
+    app.run(debug=True)
