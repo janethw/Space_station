f8fd7459f59b41e5844ecf4325613625656a5a2f
Janet Hamilton-Wilks <165585567+janethw@users.noreply.github.com>
2024-09-04 20:50:41 +0100

    Configured custom logger in logging_utils.py that can be imported and called in each module to create a logger of that module's name. The custom logger outputs to app.log and will include the module's name in the logging message. Imported the setup_logging function into main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for
-# In from logging_utils import init_logger
+from logging_utils import setup_logger
 from space_station_backend.app import create_app
 
 # creates the Flask instance in the create_app function in __init__.py
 app = create_app()
+
+# Set up logger
+logger = setup_logger(__name__)
 
 
 if __name__ == "__main__":
@@ -13,6 +16,6 @@ if __name__ == "__main__":
     # Turning off the reloader prevents the script from running twice.
     # app.run(debug=True, use_reloader=False)
 
-    # However, for dev, we want the relaoder to run, wo will leave it on,
+    # However, for dev, we want the reloader to run, wo will leave it on,
     # even if it does mean some duplicate logs.
     app.run(debug=True)
