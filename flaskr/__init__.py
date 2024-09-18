547c1ee33bafbde18d65d40dd1c61031bbf67af5
Janet Hamilton-Wilks <165585567+janethw@users.noreply.github.com>
2024-09-19 00:09:44 +0100

    Paths corrected. Getting 'unable to open db file' error. TO DO: debug db initialisation.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,7 +12,7 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_object('DevelopmentConfig')
+        app.config.from_object(DevelopmentConfig)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
