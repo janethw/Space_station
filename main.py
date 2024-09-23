3fc8cd2ef4e63f9f7faa8f043a21b8b89be0ded0
Janet Hamilton-Wilks <jhwilks@hotmail.com>
2024-09-23 09:21:38 +0100

    Typo correction in comments in main.py.

--- main.py
+++ main.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     # Turning off the reloader prevents the script from running twice.
     # app.run(debug=True, use_reloader=False)
 
-    # However, for dev, we want the reloader to run, wo will leave it on,
+    # However, for dev, we want the reloader to run, so will leave it on,
     # even if it does mean some duplicate logs.
     app.run(debug=True)
